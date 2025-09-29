e1ad6a779e0651b3e8ad6f5adf691682461c2e60
marukatze <kaffeekoenigg@gmail.com>
2025-09-29 14:12:35 +0300

    added getter and setter for AbstractProgram

--- src/task1/AbstractProgram.java
+++ src/task1/AbstractProgram.java
@@ -3,6 +3,16 @@ package task1;
 class AbstractProgram implements Runnable {
     private ProgramState state = ProgramState.UNKNOWN;
 
+    public synchronized ProgramState getState() {
+        return state;
+    }
+
+    public synchronized void setState(ProgramState newState) {
+        this.state = newState;
+        System.out.println("Состояние программы -> " + newState);
+        notifyAll();
+    }
+
     @Override
     public void run() {
         // in process
