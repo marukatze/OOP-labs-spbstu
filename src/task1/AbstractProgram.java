52a5bc2c63cb38600c47dccb830c21a877be4282
marukatze <kaffeekoenigg@gmail.com>
2025-09-29 14:11:43 +0300

    added AbstractProgram Runnable class

---
+++ src/task1/AbstractProgram.java
@@ -0,0 +1,10 @@
+package task1;
+
+class AbstractProgram implements Runnable {
+    private ProgramState state = ProgramState.UNKNOWN;
+
+    @Override
+    public void run() {
+        // in process
+    }
+}
